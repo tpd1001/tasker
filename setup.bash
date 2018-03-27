299b6aad19f693c95688b5a6780e5035343b03c0
Tom Davidson <tom.davidson@nashwan.org>
2018-03-27 09:31:11 +0100

    added import of Tasker array SETTINGS fixed $thing added Debug() configured three app's backup dirs with DEVICE support

--- setup.bash
+++ setup.bash
@@ -4,15 +4,23 @@
 #export PATH=$PATH:/data/data/burrows.apps.busybox/app_busybox; export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/data/burrows.apps.busybox/app_busybox
 
 Verbose()  { if [ "$VERBOSE" = "1" ]; then echo -e "$@" ; fi; }
+Debug(){ case "$DEBUG" in
+ 1)test -n "$1"&&echo "+++ $@";;
+ 2)test -n "$1"&&echo "+++ $@" >&2;;
+ 3)test -n "$1" && echo "+++ $@" >&2 ; return 1;;
+ x)set -x;DEBUG=;;
+esac; }
 
-thing="-"
+SETTINGS=$(echo $SETTINGS|sed 's/ /-/g' 2>/dev/null)
+DEVICE=$(echo $SETTINGS|cut -d, -f2)
+thing=" - "
 test -n "$1" && thing=$1
 for x in $(grep -i "$thing" $0 2>/dev/null | \
            sed 's/ //g;s/\*/file/' 2>/dev/null | \
            cut -d- -f2 2>/dev/null | \
            grep "^ *\/" 2>/dev/null)
 do
-d=$(dirname $x 2>/dev/null)
+d=$(dirname $x 2>/dev/null|sed "s|DEVICE|$DEVICE|")
 test -d $d && Verbose "dir exists:  $d" >&2 || {
               Verbose "created dir: $d";$FAKE mkdir -p $d; }
 done
@@ -28,15 +36,17 @@ Drivesync - /storage/emulated/0/Drivesync/backup/DrivesyncSettings.bkp
 Dropsync - /storage/emulated/0/Dropsync/backup/DropsyncSettings.bkp
 ES File Explorer - /sdcard/ESSettings.zip - (configurable)
 Fing - /storage/emulated/0/overlook/fing/backup/
-FolderSync - /sdcard/Android/data/dk.tacit.android.foldersync/files/backup/
+FolderSync - /sdcard/Android/data/dk.tacit.android.foldersync.full/files/backup/DEVICE/variable_file_name
 gReader - /sdcard/gReader/backup/
 Informant - /sdcard/PocketInformant/
 Jump - /storage/emulated/0/Download/* - (Export, use ES save to, changeable)
 LightFlow - /sdcard/lightflow/<date> - (configurable)
+List My Apps - /sdcard/AppList/DEVICE/configurable - (Export, use ES save to, changeable)
 Metam - /sdcard/Download/MetamBackup.zip - (Share with ES Save to... Download)
 Minimalistic Text - /sdcard/MinimalisticTextPreferences/* - (have to backup each widget individually, spaces in names get squashed, no global settings backup)
 MyBackup Pro - /sdcard/rerware/* - backup data, select all, deselect APNs
 Notification Toggle - /sdcard/Android/data/de.j4velin.notificationToggle/files/backup/* - (backup some settings, but should be handled by Google)
+Nova Launcher - /sdcard/data/com.teslacoilsw.launcher/backup/DEVICE/variable_file_name
 Ovuview - /sdcard/ovuview/backup/backup*.ovu
 Ping HostMonitor - /sdcard/Android/data/se.ping.android.hostmonitor/files/hosts.ser
 Podkicker Pro - /storage/emulated/0/podkicker_backup.opml
