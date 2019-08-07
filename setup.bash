db0ec5b768ca8ef8381dd5c8d5020bbd2d2eacac
Tom Davidson <tom.davidson@nashwan.org>
2019-08-07 08:32:39 +0100

    fix for modern dirname ignoring trailing slash & added more dirs

--- setup.bash
+++ setup.bash
@@ -1,3 +1,4 @@
+#export FAKE=echo
 # you can download the busybox app and run this script from within there
 # this was the original usage and required the following
 # PATH and LD_LIBRARY_PATH modifications
@@ -23,7 +24,7 @@ thing=" - "
 test -n "$1" && thing=$1
 # turn all the app backup paths/files into consistent dir names
 for x in $(grep -i "$thing" $0 2>/dev/null | \
-           sed 's/ //g;s/\*/file/' 2>/dev/null | \
+           sed 's/ //g;s/\*/file/;s|\/$|/file|' 2>/dev/null | \
            cut -d- -f2 2>/dev/null | \
            grep "^ *\/" 2>/dev/null)
 do
@@ -39,7 +40,11 @@ exit
 # a forward slash and end with a file name or forward slash
 
 App Configs From Internal Storage Root - /storage/emulated/0/AppConfigsFromIntStorageRoot/
-AutoRemote Groups - /storage/emulated/0/AutoRemoteLite/channels/
+AutoBarcode - /storage/emulated/0/AutoBarcode/
+AutoNotification - /storage/emulated/0/AutoNotification/BlockFilterForDB
+AutoRemote Groups - /storage/emulated/0/AutoRemote/channels
+AutoShare Intents - /storage/emulated/0/AutoShare/intentinfo.db
+AutoWeb Services - /storage/emulated/0/AutoWeb/Api
 Barcode Bank - /sdcard/barcodebank.xml
 Drivesync - /storage/emulated/0/Drivesync/backup/DrivesyncSettings.bkp
 Dropsync - /storage/emulated/0/Dropsync/backup/DropsyncSettings.bkp
@@ -52,6 +57,7 @@ Jump - /storage/emulated/0/Jump/JumpDesktopServers.jdz - (Export, use ES save to
 KLWP - /storage/emulated/0/Kustom/wallpapers/*
 LightFlow - /sdcard/lightflow/<date> - (configurable)
 List My Apps - /sdcard/AppList/DEVICE/configurable - (Export, use ES save to, changeable)
+Lithium Pro - /storage/emulated/0/LithiumBackups/
 Metam - /sdcard/Download/MetamBackup.zip - (Share with ES Save to... Download)
 Minimalistic Text - /sdcard/MinimalisticTextPreferences/* - (have to backup each widget individually, spaces in names get squashed, no global settings backup)
 MyBackup Pro - /sdcard/rerware/* - backup data, select all, deselect APNs & Alarms
@@ -59,12 +65,16 @@ Notification Toggle - /sdcard/Android/data/de.j4velin.notificationToggle/files/b
 Nova Launcher - /sdcard/data/com.teslacoilsw.launcher/backup/DEVICE/variable_file_name
 Ovuview - /sdcard/ovuview/backup/backup*.ovu - (Hamburger > Export, untick chart images, not ES Save To, use FileManager+, Download, ovuview-data.csv)
 Ping HostMonitor - /sdcard/Android/data/se.ping.android.hostmonitor/files/hosts.ser
+PodcastAddict - /storage/emulated/0/Android/data/com.bambuna.podcastaddict/files/podcast/backup/
 Pocket Informant - /storage/emulated/0/PocketInformant/
 Podkicker Pro - /storage/emulated/0/Android/data/com.podkicker/files/podkicker_backup.opml
 Port Knocker - /storage/emulated/0/PortKnocker/hosts-<date>-<time>.json
-Seasame Shortcuts - /storage/emulated/0/data/ninja.seasame.app.edge/backups/YYYY-MM-DD_hh.mm.ss.seasamebak
+Sesame Shortcuts - /storage/emulated/0/data/ninja.sesame.app.edge/backups/YYYY-MM-DD_hh.mm.ss.sesamebak
 Tasker - /storage/emulated/0/Tasker/userbackup.xml
+Tasker scenes - /storage/emulated/0/Tasker/scenes/*
+Tasker shared - /storage/emulated/0/Tasker/shared/*
 Tasker profiles - /storage/emulated/0/Tasker/profiles/*
+Tasker projects - /storage/emulated/0/Tasker/projects/*
 Tasker tasks - /storage/emulated/0/Tasker/tasks/*
 Termius - /storage/emulated/0/Termius/SshKeys/*
 Trigger - /sdcard/Trigger/Backup.json
